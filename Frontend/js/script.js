685945f8d3c2852f9d2c2f658faaa398bf9304cb
Evgeny Vodyannikov <vodyannikov.eugene@gmail.com>
2023-03-03 03:31:14 -0800

    refactor

--- Frontend/js/script.js
+++ Frontend/js/script.js
@@ -60,19 +60,55 @@ const fillAdvertisements = (items) => {
     let advertisementsContainer = $('div.news-box.section-padding');
 
     let advertisementsHTML = '';
+    let Item = null;
 
     $.each(items, function (index, item) {
 
-        advertisementsHTML += `<div class="new-item">
-        <div class="row">
-        <div class="col-sm-8 col-xs-12">
-        <div class="text">
-        <div class="date date-bottom">${1000}</div>
-        <div class="status">${2}</div>
-        <div class="name"><a href>${item.Title}</a></div>
-        </div>
-        </div>
-        </div>
+        Item = document.createElement('div');
+        Item.className = "news-item";
+
+        let Row = document.createElement('div');
+        Row.className = "row";
+        Item.appendChild(Row);
+
+        let LeftBlock = document.createElement('div');
+        LeftBlock.className = "col-sm-4 col-xs-12";
+        let Img = document.createElement('div');
+        Img.className = "img";
+
+        let LinkToImage = document.createElement('a');
+        LinkToImage.href = "";
+        let Image = document.createElement('img');
+        Image.src = item.Image.Url;
+        LinkToImage.appendChild(Image);
+        Img.appendChild(LinkToImage);
+        LeftBlock.appendChild(Img);
+
+        let RightBlock = document.createElement('div');
+        LeftBlock.className = "col-sm-8 col-xs-12";
+
+        Row.appendChild(LeftBlock);
+        Row.appendChild(RightBlock);
+
+
+        advertisementsHTML += `
+        
+        <div class="news-item">
+            <div class="row">
+                <div class="col-sm-4 col-xs-12">
+                    <div class="img">
+                    <a href><img src="${item.Image.Url}" alt></a>
+                    </div>
+                </div>
+
+                <div class="col-sm-8 col-xs-12">
+                    <div class="text">
+                        <div class="date date-bottom">${item.Created}</div>
+                        <div class="status">Rental</div>
+                        <div class="name"><a href>${item.Title}</a></div>
+                    </div>
+                </div>
+            </div>
         </div>`;
 
     });
